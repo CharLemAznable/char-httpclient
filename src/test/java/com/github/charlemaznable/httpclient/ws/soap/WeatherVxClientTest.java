174025444d9bb3837eef1a8417921cee96ffafd7
CharLemAznable <545541819@qq.com>
2023-09-14 17:08:23 +0800

    Some refactor of test cases.

--- src/test/java/com/github/charlemaznable/httpclient/ws/soap/WeatherVxClientTest.java
+++ src/test/java/com/github/charlemaznable/httpclient/ws/soap/WeatherVxClientTest.java
@@ -5,11 +5,14 @@ import com.github.charlemaznable.httpclient.ws.entity.GetSupportCity;
 import com.github.charlemaznable.httpclient.ws.entity.GetSupportProvince;
 import io.vertx.core.Future;
 import io.vertx.core.Vertx;
+import io.vertx.junit5.Timeout;
 import io.vertx.junit5.VertxExtension;
 import io.vertx.junit5.VertxTestContext;
 import lombok.val;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+
+import java.util.concurrent.TimeUnit;
 
 import static com.github.charlemaznable.core.lang.Listt.newArrayList;
 import static com.github.charlemaznable.httpclient.vxclient.VxFactory.vxLoader;
@@ -17,6 +20,7 @@ import static com.github.charlemaznable.httpclient.vxclient.VxFactory.vxLoader;
 @ExtendWith(VertxExtension.class)
 public class WeatherVxClientTest {
 
+    @Timeout(value = 1, timeUnit = TimeUnit.MINUTES)
     @Test
     public void testWeatherVxClient(Vertx vertx, VertxTestContext test) {
         val client = vxLoader(new VertxReflectFactory(vertx)).getClient(WeatherVxClient.class);
