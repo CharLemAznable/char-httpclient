57903f9e2da74b0790654dcd4a07fea75902127a
CharLemAznable <545541819@qq.com>
2023-09-27 20:12:13 +0800

    Add VxClient micro-meter config test.

--- src/test/java/com/github/charlemaznable/httpclient/vxclient/westcache/WestCacheConfiguration.java
+++ src/test/java/com/github/charlemaznable/httpclient/vxclient/westcache/WestCacheConfiguration.java
@@ -4,13 +4,23 @@ import com.github.bingoohuang.westcache.spring.WestCacheableEnabled;
 import com.github.bingoohuang.westcache.spring.WestCacheableScan;
 import com.github.charlemaznable.core.vertx.spring.VertxImport;
 import com.github.charlemaznable.httpclient.vxclient.VxScan;
+import io.micrometer.core.instrument.Clock;
+import io.micrometer.core.instrument.logging.LoggingMeterRegistry;
+import io.micrometer.core.instrument.logging.LoggingRegistryConfig;
+import io.vertx.core.VertxOptions;
+import io.vertx.micrometer.MicrometerMetricsOptions;
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
 import lombok.val;
+import org.jetbrains.annotations.NotNull;
 import org.n3r.diamond.client.impl.MockDiamondServer;
+import org.slf4j.LoggerFactory;
 import org.springframework.aop.framework.autoproxy.DefaultAdvisorAutoProxyCreator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.time.Duration;
+import java.util.concurrent.Executors;
 
 import static com.github.charlemaznable.configservice.diamond.DiamondFactory.diamondLoader;
 import static com.github.charlemaznable.core.spring.SpringFactory.springFactory;
@@ -24,8 +34,26 @@ import static org.joor.Reflect.on;
 @VertxImport
 public class WestCacheConfiguration {
 
+    private final LoggingMeterRegistry meterRegistry;
+
+    public WestCacheConfiguration() {
+        val meterLogger = LoggerFactory.getLogger(this.getClass().getPackageName() + ".meter");
+        this.meterRegistry = new LoggingMeterRegistry(new LoggingRegistryConfig() {
+            @Override
+            public String get(@NotNull String key) {
+                return null;
+            }
+
+            @Override
+            public @NotNull Duration step() {
+                return Duration.ofSeconds(1);
+            }
+        }, Clock.SYSTEM, meterLogger::debug);
+    }
+
     @PostConstruct
     public void postConstruct() {
+        this.meterRegistry.start(Executors.defaultThreadFactory());
         on(diamondLoader(springFactory())).field("configCache").call("invalidateAll");
         on(springVxLoader()).field("vxCache").call("invalidateAll");
         MockDiamondServer.setUpMockServer();
@@ -34,6 +62,15 @@ public class WestCacheConfiguration {
     @PreDestroy
     public void preDestroy() {
         MockDiamondServer.tearDownMockServer();
+        this.meterRegistry.stop();
+    }
+
+    @Bean
+    public VertxOptions vertxOptions() {
+        return new VertxOptions().setMetricsOptions(
+                new MicrometerMetricsOptions()
+                        .setMicrometerRegistry(this.meterRegistry)
+                        .setEnabled(true));
     }
 
     @Bean
